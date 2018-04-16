9b6ec117407d54d911573e33a5895c02e3b048f0
Alvin K <alvinck55@gmail.com>
2018-04-16 16:05:20 -0500

    Created the connection string in Web.config Updated the Data page to test a query.

--- POSTOFFICE3/Data.aspx
+++ POSTOFFICE3/Data.aspx
@@ -9,6 +9,9 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            Enter Customer ID:<br />
+            <asp:TextBox ID="customeridTextBox" runat="server"></asp:TextBox>
+            <asp:Button ID="searchButton" runat="server" OnClick="searchButton_Click" Text="Search" />
         </div>
     </form>
 </body>
--- POSTOFFICE3/Data.aspx.cs
+++ POSTOFFICE3/Data.aspx.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace POSTOFFICE3
 {
     public partial class Data : System.Web.UI.Page
     {
+        SqlConnection conn;
+        SqlCommand command;
+        SqlDataReader dataReader;
+        string sqlQuery;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void searchButton_Click(object sender, EventArgs e)
+        {
+            searchData();
+        }
+        private void searchData()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PostOffice"].ToString();
+            conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+
+                sqlQuery = "SELECT CUSTOMER.Fname FROM POSTOFFICE.CUSTOMER WHERE POSTOFFICE.CUSTOMER.Customer_ID ='" + customeridTextBox.Text + "'";
+                command = new SqlCommand(sqlQuery, conn);
+                dataReader = command.ExecuteReader();
+
+
+                dataReader.Close();
+                command.Dispose();
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                
+
+            }
 
         }
     }
