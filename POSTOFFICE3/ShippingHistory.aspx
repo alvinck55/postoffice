7b1b58eba953b8daee62945ced9aa69735a9dd41
lcarranco <lcarranco@gmail.com>
2018-04-22 19:29:37 -0500

    Working on Shipping History

--- POSTOFFICE3/ShippingHistory.aspx
+++ POSTOFFICE3/ShippingHistory.aspx
@@ -17,12 +17,37 @@
         To Date<br />
         <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
         <br />
-        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
             <asp:ListItem Value="30">Last 30 days</asp:ListItem>
-            <asp:ListItem Value="60">Lasy 60 days</asp:ListItem>
+            <asp:ListItem Value="60">Last 60 days</asp:ListItem>
             <asp:ListItem Value="90">Last 90 days</asp:ListItem>
-            <asp:ListItem>Custom</asp:ListItem>
+            <asp:ListItem Value="Custom">Custom</asp:ListItem>
         </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="Tracking_no" HeaderText="Tracking_no" SortExpression="Tracking_no" />
+                    <asp:BoundField DataField="Types" HeaderText="Types" SortExpression="Types" />
+                    <asp:BoundField DataField="Priority" HeaderText="Priority" SortExpression="Priority" />
+                    <asp:BoundField DataField="Last_updated" HeaderText="Last_updated" SortExpression="Last_updated" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:POSTOFFICE2ConnectionString %>" SelectCommand="SELECT Tracking_no, Types, Priority, Last_updated FROM POSTOFFICE2.dbo.PACKAGE p WHERE p.Sender_ID=@customerid">
+                <SelectParameters>
+                    <asp:ControlParameter ControlID="TextBox1" Name="customerid" PropertyName="Text" />
+                </SelectParameters>
+            </asp:SqlDataSource>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
     </form>
 </body>
 </html>
--- POSTOFFICE3/ShippingHistory.aspx.cs
+++ POSTOFFICE3/ShippingHistory.aspx.cs
@@ -10,53 +10,123 @@ namespace POSTOFFICE3
 {
     public partial class ShippingHistory : System.Web.UI.Page
     {
+        string username;
+        int customerid;
+        List<int> packageid = new List<int>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("Home.aspx");
+            }
+            username = Session["username"].ToString();
+
             Calendar1.SelectedDate = DateTime.Today + TimeSpan.FromDays(-30);
             Calendar2.SelectedDate = DateTime.Today;
         }
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string value = DropDownList1.SelectedItem.Value;
-            //SearchData();
+            //Label1.Text = "Entered SelextedIndexChanged";
+            string value = DropDownList1.SelectedValue;
+            if (value == "30")
+            {
+                SearchData(value);
+            }
+            if (value == "60")
+            {
 
+            }
+            if (value == "90")
+            {
+
+            }
+            if (value == "Custom")
+            {
+
+            }
         }
-        //private void SearchData()
-        //{
-        //    SqlConnection conn;
-        //    SqlCommand command;
-        //    SqlDataReader dataReader;
-        //    string sqlQuery;
-        //    string output;
+        private void SearchData(string value)
+        {
+            SqlCommand command;
+            SqlDataReader dataReader;
+            SqlConnection conn;
+            string sqlQuery;
+            string connectionString;
 
-        //    string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PostOffice"].ToString();
-        //    conn = new SqlConnection(connectionString);
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PostOffice"].ToString();
+            conn = new SqlConnection(connectionString);
 
-        //    conn.Open();
+            conn.Open();
 
-        //    sqlQuery = "SELECT * FROM POSTOFFICE2.dbo.PACKAGE WHERE Sender_ID='" + customeridTextBox.Text + "'";
-        //    command = new SqlCommand(sqlQuery, conn);
-        //    dataReader = command.ExecuteReader();
-        //    output = "";
+            sqlQuery = "SELECT * FROM POSTOFFICE2.dbo.CUSTOMER WHERE Email='" + username + "'";
+            command = new SqlCommand(sqlQuery, conn);
+            dataReader = command.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    customerid = dataReader.GetInt32(dataReader.GetOrdinal("Customer_ID"));
+                }
+                TextBox1.Text = customerid.ToString();
+            }
+            dataReader.Close();
+            command.Dispose();
+            conn.Close();
 
+            //conn = CreateConnectionString();
+            //conn.Open();
+            //sqlQuery = "SELECT * FROM POSTOFFICE2.dbo.PACKAGE p WHERE p.Sender_ID='" + customerid.ToString() + "'";
+            //command = new SqlCommand(sqlQuery, conn);
+            //dataReader = DataReader(command);
+            //if (dataReader.HasRows)
+            //{
+            //    while (dataReader.Read())
+            //    {
+            //        //packageid.Add(new Student("bob"));
+            //        //packageid.Add(new Student("joe"));
+            //        //Student joe = packageid[1];
+            //        packageid.Add(dataReader.GetInt32(dataReader.GetOrdinal("Package_ID"));
+            //    }
+            //    Label1.Text = packageid.ToString();
+            //}
+            //dataReader.Close();
+            //command.Dispose();
+            //conn.Close();
+        }
 
-        //    if (dataReader.HasRows)
-        //    {
-        //        while (dataReader.Read())
-        //        {
-        //            output = dataReader.GetString(dataReader.GetOrdinal("Fname"));
-        //        }
-        //        //Label1.Text = output;
-        //    }
-        //    else
-        //    {
-        //        //Label1.Text = "invalid employee ID";
-        //    }
+        private SqlConnection CreateConnectionString()
+        {
+            string connectionString;
+            SqlConnection conn;
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PostOffice"].ToString();
+            conn = new SqlConnection(connectionString);
+            return conn;
+        }
 
-        //    dataReader.Close();
-        //    command.Dispose();
-        //    conn.Close();
-        //}
+        private SqlCommand CreateQuery(string attribute, string table, string condition, SqlConnection conn)
+        {
+            SqlCommand command;
+            string sqlQuery;
+            string select;
+            string from;
+            string where;
+
+            select = "SELECT " + attribute;
+            from = "FROM POSTOFFICE2.dbo." + table;
+            where = "WHERE " + condition;
+
+            sqlQuery = select + from + where;
+            command = new SqlCommand(sqlQuery, conn);
+            return command;
+        }
+
+        private SqlDataReader DataReader(SqlCommand command)
+        {
+            SqlDataReader dataReader;
+            dataReader = command.ExecuteReader();
+            return dataReader;
+        }
     }
 }
